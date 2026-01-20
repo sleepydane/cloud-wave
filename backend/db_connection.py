5abd9e832b575b6052fdc325247d1e5db8350950
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2026-01-20 00:26:23 +0000

    update login function

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host='localhost',
+        host='lab-edu-rds-aurora.c106ucw2e7rz.ap-northeast-2.rds.amazonaws.com',
         database='trip_advisor',
         user='user',
         password='qwer1234'
